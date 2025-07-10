d070f2e2123ecfb713e444a7b051ecb40ecb6ff2
nbr-degenerate <siddarthevvs@gmail.com>
2025-07-10 21:58:30 +0530

    window aquired

--- TimerApp/TimerApp.cpp
+++ TimerApp/TimerApp.cpp
@@ -1,8 +1,44 @@
+//#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int WIDTH = 800, HEIGHT = 800;
+
+void frameBufferSizeCallback(GLFWwindow*, int width, int height);
+
 int main() {
-	char c;  std::cin >> c;
-	std::cout << c;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "window", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	if (!window) {
+		std::cout << "FAILED TO CREATE WINDOW\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	/*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "FAILED TO INITIALIZE GLAD";
+		glfwTerminate();
+		return -1;
+	}*/
+
+	glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+	std::cout << "here\n";
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	std::cout << "achieved greateness\n";
+
 	return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow*, int width, int height) {
+	glViewport(0, 0, width, height);
 }
